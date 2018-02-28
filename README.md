7c7d07a189ddd92636d41b39226eab81d39eaf14
Le-Roux Enslin <le-rouxe@miway.co.za>
2018-02-28 07:45:49 +0200

    * Changed background image * Added drop shadow for text as per Borderlands font styling * Added package.json export from Fitbit studio * Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ionic-watchface-borderlands
-A Borderlands themed watchface for the Fitbit Ionic
+# Fitbit Ionic watchface - Borderlands
+**A Borderlands inspired watchface for the Fitbit Ionic**
+
+Health is your heart rate. When the bar is full it's reading your heart rate, if it's empty, it's not reading any pulse.  
+Shield is your battery percentage.  
+The XP bar is your current steps towards your daily goal.  
+Your current Lv (level) is the amount of flights of stairs you've climbed today.  
+
+Fitbit install link:  
+https://gam.fitbit.com/gallery/clock/07d3316c-8550-424a-b19f-80fd2f0d0fcf
--- app/index.js
+++ app/index.js
@@ -9,16 +9,16 @@ let levelText = document.getElementById("level");
 let xpBar = document.getElementById("xp_bar");
 
 function updateSteps() {
-    currentStepsField.text = (userActivity.today.local.steps || 0)  + " /";
-    levelText.text = "Lv" + userActivity.today.local.elevationGain;
+  currentStepsField.text = (userActivity.today.local.steps || 0)  + " /";
+  levelText.text = "Lv" + userActivity.today.local.elevationGain;
 
-    if  (userActivity.goals.steps != 0) {
-        let currentSteps = (userActivity.today.local.steps || 0);
-        let stepPercentage = currentSteps / userActivity.goals.steps;
-        xpBar.width = (currentSteps < userActivity.goals.steps)
-            ? stepPercentage * 340
-            : 340;
-    }
+  if  (userActivity.goals.steps != 0) {
+    let currentSteps = (userActivity.today.local.steps || 0);
+    let stepPercentage = currentSteps / userActivity.goals.steps;
+    xpBar.width = (currentSteps < userActivity.goals.steps)
+      ? stepPercentage * 340
+      : 340;    
+  }
 }
 
 stepGoalField.text = userActivity.goals.steps || 0;
@@ -26,19 +26,21 @@ stepGoalField.text = userActivity.goals.steps || 0;
 //BATTERY
 import { battery } from "power";
 let batteryField = document.getElementById('battery');
+let batteryFieldSh = document.getElementById('batteryShadow');
 let batteryBar = document.getElementById('shield_bar');
 
 batteryField.text = Math.floor(battery.chargeLevel);
 
 function updateBattery() {
-    let batteryPercentage = Math.floor(battery.chargeLevel);
-
-    batteryField.text = batteryPercentage;
-
-    if (batteryPercentage != 0) {
-        // batteryBar.width = (batteryPercentage / 100) * 171;
-        batteryBar.x = 39 - (171 - ((batteryPercentage / 100) * 171));
-    }
+  let batteryPercentage = Math.floor(battery.chargeLevel);
+  
+  batteryField.text = batteryPercentage;
+  batteryFieldSh.text = batteryPercentage;
+  
+  if (batteryPercentage != 0) {
+    // batteryBar.width = (batteryPercentage / 100) * 171;
+    batteryBar.x = 39 - (171 - ((batteryPercentage / 100) * 171));
+  }
 }
 
 updateBattery();
@@ -48,21 +50,22 @@ battery.onchange = () => updateBattery();
 let date = document.getElementById('date');
 let dayOfWeek = document.getElementById('dayOfWeek');
 
-let monthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+let monthNames = [
+    "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
 ];
 
-let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday"];
+let dayNames = [
+    "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
 
 function updateDate() {
-    let dayInfo = new Date();
-    let day = dayInfo.getDay();
-    let month = dayInfo.getMonth();
-    let dayOfMonth = dayInfo.getDate();
-
-    date.text = `${monthNames[month]} ${dayOfMonth}`;
-    dayOfWeek.text = `${dayNames[day]}`;
+  let dayInfo = new Date();
+  let day = dayInfo.getDay();
+  let month = dayInfo.getMonth();
+  let dayOfMonth = dayInfo.getDate();
+  
+  date.text = `${monthNames[month]} ${dayOfMonth}`;
+  dayOfWeek.text = `${dayNames[day]}`; 
 }
 
 //CLOCK
@@ -72,16 +75,19 @@ import * as util from "../common/utils";
 clock.granularity = "minutes";
 
 let time = document.getElementById("time");
+let timeSh = document.getElementById("timeShadow");
 
 function updateClock() {
-    let today = new Date();
-    let hours = today.getHours();
-    hours = hours % 12;
-    hours = hours ? util.zeroPad(hours) : 12;
-    let mins = util.zeroPad(today.getMinutes());
+  let today = new Date();
+  let hours = today.getHours();
+  hours = hours % 12;
+  hours = hours ? util.zeroPad(hours) : 12;
+  let mins = util.zeroPad(today.getMinutes());
 
-    time.text = `${hours}:${mins}`;
-    updateDate();
+  time.text = `${hours}:${mins}`;
+  timeSh.text = `${hours}:${mins}`;
+  
+  updateDate();
 }
 
 clock.ontick = () => updateClock();
@@ -92,31 +98,33 @@ import { user } from "user-profile";
 
 var hrm = new HeartRateSensor();
 let hrLabel = document.getElementById("hrm");
+let hrLabelSh = document.getElementById("hrmShadow");
 let hrLevel = document.getElementById("hrLevel");
 let hrBar = document.getElementById("health_bar");
 let hrmLastTimeStamp = 0;
 
 hrLabel.text = "??";
 let hrCustomZoneNames = {
-    'out-of-range': 'Relaxed',
-    'fat-burn': 'Fat Burn',
-    'cardio': 'Cardio',
-    'peak': 'Peak'
+  'out-of-range': 'Relaxed',
+  'fat-burn': 'Fat Burn',
+  'cardio': 'Cardio',
+  'peak': 'Peak'
 };
 
 hrm.onreading = function(read) {
-    let heartRate = hrm.heartRate;
-    let hrZone = user.heartRateZone(heartRate);
-
-    hrLabel.text = heartRate;
-    hrLevel.text = hrCustomZoneNames[`${hrZone}`];
-    hrBar.x = 39; //207
-
-    hrm.stop();
+  let heartRate = hrm.heartRate;
+  let hrZone = user.heartRateZone(heartRate);
+  
+  hrLabel.text = heartRate;
+  hrLabelSh.text = heartRate;
+  hrLevel.text = hrCustomZoneNames[`${hrZone}`];
+  hrBar.x = 39; //207
+  
+  hrm.stop();
 }
 
 hrm.onerror = function() {
-    hrLabel.text = '??';
+  hrLabel.text = '??';
 }
 
 hrm.start();
@@ -126,25 +134,23 @@ import { display } from "display";
 setInterval(intervalFunction, 2500);
 
 function intervalFunction() {
-    if (display.on) {
-        hrm.start();
-    }
-
+  if (display.on) {
+    hrm.start();
     updateSteps();
 
     if (hrmLastTimeStamp == hrm.timestamp) {
-        hrLabel.text = '??';
         hrLevel.text = "Couch Potato";
         hrBar.x = -168;
     } else {
-        hrmLastTimeStamp = hrm.timestamp;
+        hrmLastTimeStamp = hrm.timestamp; 
     }
+  }
 }
 
 display.onchange = function(event) {
-    if (display.on) {
-        hrm.start();
-    } else {
-        hrm.stop();
-    }
+  if (display.on) {
+    hrm.start();
+  } else {
+    hrm.stop();
+  }
 };
